7b2e6d91d742a14fdc143695d44683a85299da70
Gustavo <gustavogod007boss@gmail.com>
2026-07-27 13:47:36 -0600

    Registra los modelos en el panel de administracion

--- escuela/academico/admin.py
+++ escuela/academico/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from academico.models import Carrera, Alumno, Materia, Inscripcion 
 
+admin.site.register(Carrera)
+admin.site.register(Alumno)
+admin.site.register(Materia)
+admin.site.register(Inscripcion) 
 # Register your models here.
