37774e237da6264ca3a5719966133d5d64db0cb4
Gustavo <gustavogod007boss@gmail.com>
2026-07-27 13:38:09 -0600

    Agrega ViewSets de Carrera, Alumno, Materia e Inscripcion

--- escuela/academico/views.py
+++ escuela/academico/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from academico.models import Carrera, Alumno, Materia, Inscripcion
+from academico.api.serializers import ( 
+CarreraSerializer, AlumnoSerializer,
+MateriaSerializer, InscripcionSerializer,
+) 
+
+class CarreraViewSet(viewsets.ModelViewSet): 
+    queryset = Carrera.objects.all()
+    serializer_class = CarreraSerializer
+    
+class AlumnoViewSet(viewsets.ModelViewSet):
+       queryset = Alumno.objects.all()
+       serializer_class = AlumnoSerializer
+       
+class MateriaViewSet(viewsets.ModelViewSet):
+           queryset = Materia.objects.all()
+           serializer_class = MateriaSerializer
+           
+class InscripcionViewSet(viewsets.ModelViewSet): 
+               queryset = Inscripcion.objects.all() 
+               serializer_class = InscripcionSerializer 
+
 # Create your views here.
